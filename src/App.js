046a43681a3a43ddd31187b2ce5b00eb6f478697
Latch Jackn <latch.jack@gmail.com>
2020-11-10 16:17:59 +0000

    added styled component styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -81,12 +97,9 @@ class App extends Component {
       <div className="App">
         <h1>React App</h1>
         <p className={classes.join(' ')}>This is working!</p>
-        <button
-          style={style}
-          onClick={this.togglePeopleHandler}
-        >
+        <StyledButton alt={this.state.showPeople} onClick={this.togglePeopleHandler}>
           Toggle People
-        </button>
+        </StyledButton>
         {people}
       </div>
     );
