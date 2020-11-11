52b2e4c991738031c58bd588f184800a65e6be5c
Latch Jackn <latch.jack@gmail.com>
2020-11-11 15:31:14 +0000

    added lifecycle hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,20 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
   state = {
     persons: [
       { id: 'asfa1', name: 'Max', age: 28 },
@@ -46,6 +60,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
     if (this.state.showPersons) {
       persons = <Persons
