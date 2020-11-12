73e27a7f5e3dcb6cc8beebe5947c1dc78866d1e4
Latch Jackn <latch.jack@gmail.com>
2020-11-12 15:47:21 +0000

    updated

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -9,7 +9,15 @@ class Persons extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[Persons.js] shouldComponentUpdate');
-    return true;
+    if (
+      nextProps.persons !== this.props.persons ||
+      nextProps.changed !== this.props.changed ||
+      nextProps.clicked !== this.props.clicked
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
