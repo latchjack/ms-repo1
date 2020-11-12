eb6cf5904170e689c0c3522a220264fcde925054
Latch Jackn <latch.jack@gmail.com>
2020-11-12 13:58:14 +0000

    added timer

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -4,10 +4,11 @@ import classes from './Cockpit.css'
 const Cockpit = (props) => {
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       alert('Save data to cloud');
     }, 1000);
     return () => {
+      clearTimeout(timer);
       console.log('[Cockpit.js] clean up in useEffect');
     }
   }, []);
