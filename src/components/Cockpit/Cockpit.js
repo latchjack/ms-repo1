4df97fa5f7977ce2ae00050ee2d22d85aba46437
Latch Jackn <latch.jack@gmail.com>
2020-11-12 11:20:49 +0000

    cleaning up methods

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css'
 
 const Cockpit = (props) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    setTimeout(() => {
+      alert('Save data to cloud');
+    }, 1000);
+  }, [props.persons]);
+
   const assignedClasses = [];
   let btnClass = '';
   if (props.showPersons) {
--- src/containers/App.js
+++ src/containers/App.js
@@ -18,6 +18,15 @@ class App extends Component {
     console.log('[App.js] componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
   state = {
     persons: [
       { id: 'asfa1', name: 'Max', age: 28 },
