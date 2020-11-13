0bc1993348bf976944b91676d49fe994ef88cd9a
Latch Jackn <latch.jack@gmail.com>
2020-11-13 12:22:29 +0000

    updated the set state in App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -37,7 +37,8 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   nameChangedHandler = (event, id) => {
@@ -56,7 +57,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: this.state.changeCounter + 1
+      };
+    });
   };
 
   deletePersonHandler = personIndex => {
